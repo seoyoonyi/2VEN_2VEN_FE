e0a20ebed688e6e2d80a7ec6a18e158bbf390603
Hyejin <hyejin.frontend@gmail.com>
2024-11-07 23:53:25 +0900

    fix: /mocks/handlers/index.ts에서 사용하지 않는 import 제거

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,3 @@
 import { authHandlers } from '@/mocks/handlers/auth.handlers';
-import { strategyHandlers } from '@/mocks/handlers/strategy.handlers';
-import { userHandlers } from '@/mocks/handlers/user.handlers';
 
-export const handlers = [...authHandlers, ...userHandlers, ...strategyHandlers];
+export const handlers = [...authHandlers];
