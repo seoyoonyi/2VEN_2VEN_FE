008fa1bf1b980ab088c67b7edc0f20fb7ac46d8f
Hyejin <hyejin.frontend@gmail.com>
2024-11-07 23:51:40 +0900

    fix: msw 세팅 후 빌드오류 수정
    - 사용하지 않는 파일의 코드를 모두 주석처리

--- src/mocks/handlers/user.handlers.ts
+++ src/mocks/handlers/user.handlers.ts
@@ -1,10 +1,10 @@
-import { http, HttpResponse } from 'msw';
+// import { http, HttpResponse } from 'msw';
 
-export const userHandlers = [
-  // http.get('/api/users/:id', async ({ params }) => {
-  //   // 사용자 정보 조회 핸들러 로직
-  // }),
-  //   http.patch('/api/users/:id', async ({ request, params }) => {
-  //     // 사용자 정보 수정 핸들러 로직
-  //   }),
-];
+// export const userHandlers = [
+//   http.get('/api/users/:id', async ({ params }) => {
+//     // 사용자 정보 조회 핸들러 로직
+//   }),
+//   http.patch('/api/users/:id', async ({ request, params }) => {
+//     // 사용자 정보 수정 핸들러 로직
+//   }),
+// ];
