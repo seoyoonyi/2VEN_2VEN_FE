1a17951409180b546694e96783e479f14d1acf25
Hyejin <hyejin.frontend@gmail.com>
2024-11-11 17:35:48 +0900

    test: input test 페이지 추가
    - input test 페이지가 임시로 HomePage에 추가되었습니다.
    - HomePage 개발 시 import 된 부분과, 해당 테스트페이지 삭제하시면 됩니다.
    - input test 페이지는 추후 삭제될 예정입니다.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,11 @@
+import InputTestPage from '@/pages/test-page/InputTestPage';
+
 const HomePage = () => (
   <div>
     <h1>메인페이지</h1>
+    <div>
+      <InputTestPage />
+    </div>
   </div>
 );
 export default HomePage;
