b301abf21d45e1612ba1f601edb662f66f6e3f2a
Sumin <tnalsdldkr2@naver.com>
2024-11-12 08:46:44 +0900

    feat: home에서 testpage 삭제 (#23)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,10 @@
 import InputTestPage from '@/pages/test-page/InputTestPage';
-import ModalTestPage from '@/pages/test-page/ModalTestPage';
 
 const HomePage = () => (
   <div>
     <h1>메인페이지</h1>
     <div>
-      {/* <InputTestPage /> */}
-      <ModalTestPage />
+      <InputTestPage />
     </div>
   </div>
 );
