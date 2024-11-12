3956f42d1552e1e58aff66f3a1d866581a9bf3c5
Sumin <tnalsdldkr2@naver.com>
2024-11-12 18:30:53 +0900

    feat: Select 컴포넌트 구현 (#24)

---
+++ src/components/common/SelectBox.tsx
@@ -0,0 +1,163 @@
+import { useState } from 'react';
+
+import { css } from '@emotion/react';
+import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/react';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+
+import theme from '@/styles/theme';
+
+type SelectType = 'sm' | 'md';
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface SelectBoxProps {
+  options: Option[];
+  type?: SelectType;
+  onSelect?: (option: Option) => void;
+  width?: string;
+  disabled?: boolean;
+}
+
+const defaultOption = { label: '선택', value: '' };
+
+const SelectBox = ({
+  options,
+  type = 'md',
+  onSelect,
+  width = '200px',
+  disabled = false,
+}: SelectBoxProps) => {
+  const [selected, setSelected] = useState<Option>(defaultOption);
+
+  const handleSelect = (option: Option) => {
+    setSelected(option);
+    if (onSelect) onSelect(option);
+  };
+
+  return (
+    <div css={selectContainerStyle(width)}>
+      <Listbox value={selected} onChange={handleSelect} disabled={disabled}>
+        {({ open }) => (
+          <>
+            <ListboxButton css={selectButtonStyle(type, open, disabled, selected)}>
+              <span css={textEllipsisStyle}>{selected.label}</span>
+              {open ? (
+                <FaChevronUp css={iconStyle(type)} />
+              ) : (
+                <FaChevronDown css={iconStyle(type)} />
+              )}
+            </ListboxButton>
+
+            <ListboxOptions css={selectOptionsStyle(type)}>
+              {options.map((option) => (
+                <ListboxOption key={option.value} value={option} css={selectOptionStyle(type)}>
+                  <span>{option.label}</span>
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </>
+        )}
+      </Listbox>
+    </div>
+  );
+};
+
+const selectContainerStyle = (width: string) => css`
+  position: relative;
+  width: ${width};
+`;
+
+const selectButtonStyle = (
+  type: SelectType,
+  open: boolean,
+  disabled: boolean,
+  selected: Option
+) => css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: ${type === 'sm' ? theme.input.height.sm : theme.input.height.md};
+  color: ${disabled
+    ? theme.colors.gray[400]
+    : selected.value !== ''
+      ? theme.colors.gray[700]
+      : theme.colors.gray[500]};
+  font-size: ${type === 'sm' ? theme.input.fontSize.sm : theme.input.fontSize.md};
+  font-weight: ${theme.typography.fontWeight.regular};
+  line-height: ${theme.typography.lineHeights.md};
+  background-color: ${disabled
+    ? theme.colors.gray[100]
+    : open
+      ? theme.colors.gray[50]
+      : theme.colors.main.white};
+  border: 1px solid ${selected.value === '' ? theme.colors.gray[300] : theme.colors.gray[400]};
+  padding: ${type === 'sm' ? '10px 7.5px' : '12px 16px'};
+  min-width: 80%;
+  cursor: ${disabled ? 'not-allowed' : 'pointer'};
+
+  &:hover {
+    background-color: ${!disabled && !open ? theme.colors.gray[50] : ''};
+  }
+
+  &:focus {
+    border-color: ${selected.value === '' ? theme.colors.gray[300] : theme.colors.gray[400]};
+  }
+`;
+
+const textEllipsisStyle = css`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+const iconStyle = (type: SelectType) => css`
+  width: ${type === 'sm' ? '8px' : '12px'};
+  color: ${theme.colors.gray[400]};
+`;
+
+const selectOptionsStyle = (type: SelectType) => css`
+  position: absolute;
+  width: 100%;
+  max-height: 144px;
+  font-size: ${type === 'sm'
+    ? theme.typography.fontSizes.caption
+    : theme.typography.fontSizes.body};
+  color: ${theme.colors.gray[500]};
+  font-weight: ${theme.typography.fontWeight.regular};
+  line-height: ${theme.typography.lineHeights.md};
+  background-color: ${theme.colors.main.white};
+  border: 1px solid ${theme.colors.gray[300]};
+  margin-top: 8px;
+  z-index: 10;
+  box-shadow: 0px 0px 6px 0px rgba(75, 85, 99, 0.1);
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 4px;
+    height: 40px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${theme.colors.gray[200]};
+  }
+`;
+
+const selectOptionStyle = (type: SelectType) => css`
+  cursor: pointer;
+  display: block;
+  padding: ${type === 'sm' ? '10px 7.5px' : '12px 16px'};
+  &:hover {
+    background-color: ${theme.colors.teal[50]};
+    color: ${theme.colors.teal[600]};
+  }
+`;
+
+export default SelectBox;
---
+++ src/pages/test-page/SelectTestPage.tsx
@@ -0,0 +1,56 @@
+import SelectBox from '@/components/common/SelectBox';
+
+const operations = [
+  { label: '자동', value: 'auto' },
+  { label: '반자동(하이브리드)', value: 'hybrid' },
+  { label: '수동(매뉴얼)', value: 'manual' },
+];
+
+const cycles = [
+  { label: '데이', value: 'day' },
+  { label: '포지션', value: 'position' },
+];
+
+const investmentFunds = [
+  { label: '1만원 ~ 500만원', value: '1-500' },
+  { label: '500만원 ~ 1000만원', value: '500-1000' },
+  { label: '1000만원 ~ 2000만원', value: '1000-2000' },
+  { label: '2000만원 ~ 5000만원', value: '2000-5000' },
+  { label: '5000만원 ~ 1억', value: '5000-10000' },
+  { label: '1억 ~ 2억', value: '10000-20000' },
+  { label: '2억 ~ 3억', value: '20000-30000' },
+  { label: '3억 ~ 4억', value: '30000-40000' },
+  { label: '4억 ~ 5억', value: '40000-50000' },
+  { label: '5억 ~ 10억', value: '50000-100000' },
+  { label: '10억 이상', value: '100000+' },
+];
+
+const isPublic = [
+  { label: '공개', value: 'public' },
+  { label: '비공개', value: 'private' },
+];
+
+const SelectTestPage = () => {
+  const handleSelect = (option: { label: string; value: string }) => {
+    console.log(option);
+  };
+
+  return (
+    <>
+      <div style={{ display: 'flex', width: '100vh', gap: '20px', marginBottom: '20px' }}>
+        <SelectBox options={operations} onSelect={handleSelect} />
+        <SelectBox options={cycles} onSelect={handleSelect} />
+        <SelectBox options={investmentFunds} onSelect={handleSelect} width='300px' />
+        <SelectBox options={isPublic} onSelect={handleSelect} disabled={true} />
+      </div>
+      <div style={{ display: 'flex', width: '100vh', gap: '20px', height: '100vh' }}>
+        <SelectBox options={operations} onSelect={handleSelect} type='sm' width='100px' />
+        <SelectBox options={cycles} onSelect={handleSelect} type='sm' disabled={true} />
+        <SelectBox options={investmentFunds} onSelect={handleSelect} type='sm' />
+        <SelectBox options={isPublic} onSelect={handleSelect} type='sm' width='80px' />
+      </div>
+    </>
+  );
+};
+
+export default SelectTestPage;
