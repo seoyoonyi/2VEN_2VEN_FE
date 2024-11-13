b642d81abd0e5ec703c69bdbbbe3b1d906bb1f51
Hyejin <hyejin.frontend@gmail.com>
2024-11-14 03:54:04 +0900

    feat: 약관 동의 컴포넌트 추가 (#33)
    - components/terms 디렉토리 추가
        - TermsCheckbox.tsx : 약관 동의 체크박스 컴포넌트
        - TermsContainer.tsx : 약관 동의 컨테이너 컴포넌트 (체크박스, 약관 컨텐츠)
        - contents 디렉토리 추가 : 약관 동의 컨텐츠 컴포넌트(투자자, 개인정보, 트레이더)
            - InvestorTerms.tsx
            - PrivacyTerms.tsx
            - TraderTerms.tsx
            - index.ts
    
    - constants/signup.ts : 약관 동의 관련 상수 추가
    - types/signup.ts : 약관 동의 관련 타입 추가
    
    ✨ TermsContainer 컴포넌트가 SignUpPage에 추가
    ✨ 각 약관 동의 컨텐트 컴포넌트에 대한 콘텐츠삽입 및 스타일 작성 필요

---
+++ src/components/terms/contents/PrivacyTerms.tsx
@@ -0,0 +1,21 @@
+export const PrivacyTerms = () => (
+  <div>
+    <h1>개인정보 이용약관</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur, velit eget
+      tincidunt ultricies, libero nisi ultricies odio, nec ultricies elit turpis sit amet elit.
+      Nullam nec consectetur lorem. Nullam nec consectetur lorem. Nullam necLorem ipsum dolor sit
+      amet, consectetur adipiscing elit. Nulla consectetur, velit eget tincidunt ultricies, libero
+      nisi ultricies odio, nec ultricies elit turpis sit amet elit. Nullam nec consectetur lorem.
+      Nullam nec consectetur lorem. Nullam necLorem ipsum dolor sit amet, consectetur adipiscing
+      elit. Nulla consectetur, velit eget tincidunt ultricies, libero nisi ultricies odio, nec
+      ultricies elit turpis sit amet elit. Nullam nec consectetur lorem. Nullam nec consectetur
+      lorem. Nullam necLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur,
+      velit eget tincidunt ultricies, libero nisi ultricies odio, nec ultricies elit turpis sit amet
+      elit. Nullam nec consectetur lorem. Nullam nec consectetur lorem. Nullam necLorem ipsum dolor
+      sit amet, consectetur adipiscing elit. Nulla consectetur, velit eget tincidunt ultricies,
+      libero nisi ultricies odio, nec ultricies elit turpis sit amet elit. Nullam nec consectetur
+      lorem. Nullam nec consectetur lorem. Nullam nec
+    </p>
+  </div>
+);
---
+++ src/components/terms/index.ts
@@ -0,0 +1,4 @@
+// 약관 컴포넌트들 export
+
+export * from '@/components/terms/contents';
+export * from '@/components/terms/TermsContainer';
