5e7f40d25cfeb59f99b12085f5e929fde9370b65
Hyejin <hyejin.frontend@gmail.com>
2024-11-13 21:10:59 +0900

    fix: RootLayout.tsx에서 레이아웃 최소높이 수정 (#16)

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -3,12 +3,11 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from '@/components/common/Footer';
 import Header from '@/components/common/Header';
-import theme from '@/styles/theme';
 
 const RootLayout = () => (
   <div css={wrapperStyle}>
     <Header />
-    <main css={mainStyle}>
+    <main>
       <Outlet />
     </main>
     <Footer />
@@ -19,8 +18,4 @@ export default RootLayout;
 
 const wrapperStyle = css`
   min-height: 100vh;
-`;
-const mainStyle = css`
-  max-width: ${theme.layout.width.content};
-  margin: 0 auto;
 `;
