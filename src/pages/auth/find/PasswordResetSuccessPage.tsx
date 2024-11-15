5bf78af1fa2b5149cc82ac35a2abdf905a1162e7
Hyejin <hyejin.frontend@gmail.com>
2024-11-16 04:24:25 +0900

    design: 비밀번호 변경 완료 페이지 생성 (#81)
    - 페이지 이미지 추가
    - 비밀번호 변경 완료 페이지 생성

--- src/pages/auth/find/PasswordResetSuccessPage.tsx
+++ src/pages/auth/find/PasswordResetSuccessPage.tsx
@@ -1,7 +1,54 @@
+import { css } from '@emotion/react';
+import { Link } from 'react-router-dom';
+
+import resetSuccessImage from '@/assets/images/resetpass_success.png';
+import Button from '@/components/common/Button';
+import { ROUTES } from '@/constants/routes';
+import theme from '@/styles/theme';
+
 const PasswordResetSuccessPage = () => (
-  <div>
-    <h1>비밀번호 재설정 성공</h1>
-    <p>비밀번호 재설정이 성공적으로 완료되었습니다.</p>
+  <div css={containerStyle}>
+    <h3 css={pageHeadingStyle}>비밀번호가 변경되었어요!</h3>
+    <img src={resetSuccessImage} alt='비밀번호 변경완료' />
+    <p>안전한 로그인을 위해 새 비밀번호로 다시 로그인해 주세요</p>
+    <Button width={154}>
+      <Link to={ROUTES.AUTH.SIGNIN}>로그인 하러가기</Link>
+    </Button>
   </div>
 );
+
+const containerStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  padding: 160px 0 240px;
+  margin: 0 auto;
+  img {
+    width: 77px;
+    margin-bottom: 24px;
+    order: 1;
+  }
+  p {
+    order: 3;
+
+    color: ${theme.colors.gray[700]};
+    font-size: ${theme.typography.fontSizes.body};
+    line-height: ${theme.typography.lineHeights.md};
+    font-weight: ${theme.typography.fontWeight.medium};
+    margin-bottom: 32px;
+  }
+
+  button {
+    order: 4;
+  }
+`;
+const pageHeadingStyle = css`
+  font-size: ${theme.typography.fontSizes.heading.h3};
+  line-height: ${theme.typography.lineHeights.md};
+  font-weight: ${theme.typography.fontWeight.bold};
+  margin-bottom: 10px;
+  order: 2;
+`;
 export default PasswordResetSuccessPage;
