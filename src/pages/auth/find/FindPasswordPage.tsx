da31c990c09ad6725afcc0ed6ad0c6ec031b4946
Hyejin <hyejin.frontend@gmail.com>
2024-11-16 02:39:44 +0900

    chore: 인증요청 입력창 및 버튼을 form 태그로 감싸기
    + button type="submit"

--- src/pages/auth/find/FindPasswordPage.tsx
+++ src/pages/auth/find/FindPasswordPage.tsx
@@ -7,33 +7,50 @@ import Input from '@/components/common/Input';
 import { ROUTES } from '@/constants/routes';
 import theme from '@/styles/theme';
 
-const FindPasswordPage = () => (
-  <div css={containerStyle}>
-    <h3 css={pageHeadingStyle}>비밀번호 찾기</h3>
-    <p css={infoStyle}>
-      <img src={tickImage} alt='tick' />
-      <span>시스메틱에 가입한 이메일을 정확히 입력해주세요</span>
-    </p>
-    <div css={emailVerifyContainer}>
-      <Input type='email' inputSize='lg' placeholder='1234@naver.com' />
-      <Button variant='accent' size='md' width={100}>
-        인증요청
-      </Button>
+const FindPasswordPage = () => {
+  const handleEmailVerification = () => {
+    // 이메일 인증 요청 로직
+  };
+  const handleCodeVerification = () => {
+    // 이메일 인증코드 확인
+  };
+  return (
+    <div css={containerStyle}>
+      <h3 css={pageHeadingStyle}>비밀번호 찾기</h3>
+      <p css={infoStyle}>
+        <img src={tickImage} alt='tick' />
+        <span>시스메틱에 가입한 이메일을 정확히 입력해주세요</span>
+      </p>
+      <form onSubmit={handleEmailVerification}>
+        <div css={emailVerifyContainer}>
+          <Input type='email' inputSize='lg' required name='email' placeholder='1234@naver.com' />
+          <Button type='submit' variant='accent' size='md' width={100}>
+            인증요청
+          </Button>
+        </div>
+      </form>
+      <form css={formStyle} onSubmit={handleCodeVerification}>
+        <Input
+          type='text'
+          inputSize='lg'
+          required
+          pattern='\d{6}' // 6자리 숫자만 입력 가능
+          name='verificationCode'
+          placeholder='메일로 받은 인증번호 입력'
+        />
+        <Button type='submit' width={400} disabled>
+          확인
+        </Button>
+      </form>
+      <ul css={findEmailLinkStyle}>
+        <li>이메일이 기억나지 않나요?</li>
+        <li>
+          <Link to={ROUTES.AUTH.FIND.EMAIL}>이메일 찾기</Link>
+        </li>
+      </ul>
     </div>
-    <form css={formStyle}>
-      <Input type='text' inputSize='lg' placeholder='메일로 받은 인증번호 입력' />
-      <Button width={400} disabled>
-        확인
-      </Button>
-    </form>
-    <ul css={findEmailLinkStyle}>
-      <li>이메일이 기억나지 않나요?</li>
-      <li>
-        <Link to={ROUTES.AUTH.FIND.EMAIL}>이메일 찾기</Link>
-      </li>
-    </ul>
-  </div>
-);
+  );
+};
 
 const containerStyle = css`
   display: flex;
