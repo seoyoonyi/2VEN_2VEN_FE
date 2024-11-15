ff8a19f3c6ddc95bce048367d851c1da955868a5
Hyejin <hyejin.frontend@gmail.com>
2024-11-16 00:29:24 +0900

    chore: 로그인페이지에서 파단 찾기링크 마크업 변경
    react-router-dom Link 사용

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,36 +1,53 @@
 import { css } from '@emotion/react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
+import { ROUTES } from '@/constants/routes';
 import theme from '@/styles/theme';
 
-const SignInPage: React.FC = () => (
-  <div css={containerStyle}>
-    <h3 css={pageHeadingStyle}>로그인</h3>
-    <form css={formStyle}>
-      <div>
-        <Input type='email' inputSize='md' leftIcon='mail' placeholder='이메일' showClearButton />
-      </div>
-      <div>
-        <Input
-          type='password'
-          inputSize='md'
-          leftIcon='key'
-          rightIcon='eye'
-          placeholder='비밀번호'
-        />
-        <Button width={400} css={buttonStyle} disabled>
-          로그인
-        </Button>
-      </div>
-    </form>
-    <ul css={signinLinkStyle}>
-      <li>아이디 찾기</li>
-      <li>비밀번호 찾기</li>
-      <li>회원가입</li>
-    </ul>
-  </div>
-);
+const SignInPage: React.FC = () => {
+  const navigate = useNavigate();
+  const handleFindEmail = () => {
+    navigate(ROUTES.AUTH.FIND.EMAIL);
+  };
+  const handleFindPass = () => {
+    navigate(ROUTES.AUTH.FIND.PASSWORD);
+  };
+  return (
+    <div css={containerStyle}>
+      <h3 css={pageHeadingStyle}>로그인</h3>
+      <form css={formStyle}>
+        <div>
+          <Input type='email' inputSize='md' leftIcon='mail' placeholder='이메일' showClearButton />
+        </div>
+        <div>
+          <Input
+            type='password'
+            inputSize='md'
+            leftIcon='key'
+            rightIcon='eye'
+            placeholder='비밀번호'
+          />
+          <Button width={400} css={buttonStyle} disabled>
+            로그인
+          </Button>
+        </div>
+      </form>
+      <ul css={signinLinkStyle}>
+        <li>
+          <Link to={ROUTES.AUTH.FIND.EMAIL}>아이디 찾기</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.AUTH.FIND.PASSWORD}>비밀번호 찾기</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.AUTH.SIGNUP.FORM}>회원가입</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 const containerStyle = css`
   display: flex;
   flex-direction: column;
@@ -74,7 +91,6 @@ const signinLinkStyle = css`
     font-size: ${theme.typography.fontSizes.body};
     line-height: ${theme.typography.lineHeights.lg};
     font-weight: ${theme.typography.fontWeight.bold};
-    cursor: pointer;
     padding: 0 17px;
 
     &::after {
