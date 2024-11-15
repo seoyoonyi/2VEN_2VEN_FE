4124439b1bacd45ad12eeac7cab5cc4d395e3b3a
jizero <jizero27@gmail.com>
2024-11-16 02:55:06 +0900

    design: 관리자 네브바 구현, 관리자페이지 레이아웃 설정 (#105)

---
+++ src/layouts/AdminLayout.tsx
@@ -0,0 +1,31 @@
+import { css } from '@emotion/react';
+import { Outlet } from 'react-router-dom';
+
+import Footer from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+import AdminNav from '@/components/navigation/AdminNav';
+import theme from '@/styles/theme';
+
+const AdminLayout = () => (
+  <div css={wrapperStyle}>
+    <Header />
+    <main css={mainStyle}>
+      <AdminNav />
+      <Outlet />
+    </main>
+    <Footer />
+  </div>
+);
+
+const wrapperStyle = css`
+  min-height: 100vh;
+  background-color: ${theme.colors.gray[100]};
+`;
+
+const mainStyle = css`
+  max-width: ${theme.layout.width.content};
+  display: flex;
+  gap: 20px;
+  margin: 76px auto;
+`;
+export default AdminLayout;
