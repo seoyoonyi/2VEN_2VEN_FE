61e8b4657ec409a2222f45de9d469883966700ca
jizero <jizero27@gmail.com>
2024-11-16 18:03:06 +0900

    feat: 파일 다운로드 컴포넌트 생성 (#60)

--- src/components/page/strategy-detail/FileDownSection.tsx
+++ src/components/page/strategy-detail/FileDownSection.tsx
@@ -1,13 +1,58 @@
 import { css } from '@emotion/react';
+import { PiFile } from 'react-icons/pi';
 
 import theme from '@/styles/theme';
 
-const FileDownSection = () => <div css={fileDownStyle}></div>;
+interface FileInfoProps {
+  fileUrl: string;
+}
+
+const FileDownSection = ({ fileUrl }: FileInfoProps) => {
+  const fileName = fileUrl.split('/').pop() || '전략파일';
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = fileName;
+    link?.click();
+  };
+
+  return (
+    <div css={fileDownStyle}>
+      <div css={nameStyle}>
+        <PiFile />
+        {fileName}
+      </div>
+      <button onClick={handleDownload}>다운로드</button>
+    </div>
+  );
+};
 
 const fileDownStyle = css`
   display: flex;
-  padding: 20 24px;
+  padding: 20px 24px;
+  margin: 32px 0px;
   align-items: flex-start;
   border: 1px solid ${theme.colors.gray[300]};
+  justify-content: space-between;
+
+  button {
+    display: flex;
+    align-items: center;
+    height: 22px;
+    width: 56px;
+    background-color: inherit;
+    color: ${theme.colors.gray[500]};
+    ${theme.typography.fontSizes.body};
+    ${theme.typography.fontWeight.bold};
+    ${theme.typography.lineHeights.md};
+  }
+`;
+
+const nameStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  ${theme.textStyle.body.body3};
 `;
 export default FileDownSection;
