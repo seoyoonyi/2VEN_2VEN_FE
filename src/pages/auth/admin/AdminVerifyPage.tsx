86f6b2cbd6f23b5050ca989e4a7eafa424abfaf7
Hyejin <hyejin.frontend@gmail.com>
2024-11-17 04:38:42 +0900

    feat: 관리자 인증 페이지 UI 기능 구현 (#106)
    - 관리자 인증 페이지 마운트 되면 타이머 시작
    - (메인페이지의 관리자인증 버튼 클릭 시 이메일인증 요청API 호출)
    - 인증번호 입력되면 확인버튼 활성화
    - 유효하지 않은 인증번호 입력 시 하단에 에러메시지 출력
    - 이때 유효시간이 끝나면 확인버튼 비활성화 및 하단에 에러메시지 출력이 덮어씌워짐
    - 타이머 종료 시간은 3분으로 설정
    - 타이머 동작 중에 재전송버튼 클릭하면,
        - 타이머 재시작
        - 이메일인증 요청API 재호출
        - 하단에 재전송 완료 메시지 출력
    - 유효시간 안에 인증번호 입력 후, 확인버튼 클릭하면
        - 유효성검사 성공 시, 관리자 전략승인관리 페이지로 이동
        - 유효성검사 실패 시, 하단에 에러메시지 출력

--- src/pages/auth/admin/AdminVerifyPage.tsx
+++ src/pages/auth/admin/AdminVerifyPage.tsx
@@ -1,42 +1,100 @@
 import { useEffect, useState } from 'react';
 
 import { css } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
 
+import Button from '@/components/common/Button';
 import VerificationInput from '@/components/page/signup/VerificationInput';
+import { ROUTES } from '@/constants/routes';
 import theme from '@/styles/theme';
+import { validateCode } from '@/utils/validation';
 
 const AdminVerifyPage = () => {
-  const [isVerificationActive, setIsVerificationActive] = useState<boolean>(false);
+  const navigate = useNavigate();
   const [verificationCode, setVerificationCode] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>('');
+  const [resetTimer, setResetTimer] = useState<number>(0); // 타이머 리셋을 위한 상태
+  const [shouldReset, setShouldReset] = useState(false);
+  const [isInputDisabled, setIsInputDisabled] = useState(false);
 
-  // 페이지 마운트하면 자동으로 인증번호 요청
+  // 재전송 처리를 위한 useEffect
   useEffect(() => {
-    requestAdminVerification();
-  }, []);
+    if (shouldReset) {
+      setErrorMessage('');
+      setResetTimer((prev) => prev + 1);
+      setIsInputDisabled(false); // 입력창 활성화
+      setShouldReset(false);
+    }
+  }, [shouldReset]);
+
+  // verificationCode가 변경될 때마다 실행
+  useEffect(() => {
+    // 입력값이 비어있다면 에러메시지 제거
+    if (!verificationCode) {
+      setErrorMessage('');
+    }
+  }, [verificationCode]);
 
   // 인증번호 재요청
-  const requestAdminVerification = () => {
+  const handleResend = () => {
+    setShouldReset(true);
+    // setState는 비동기이므로, 이 시점에서는 아직 errorMessage가 변경되지 않았음
     try {
-      // 인증번호 요청 로직
-      setIsVerificationActive(true);
-      setErrorMessage('');
+      // await requestVerificationEmail(); // 관리자 이메일로 인증번호 요청 API
+
+      // 인증번호 요청 성공 시, isVerificationActive를 토글 => 타이머 재시작!
+      setResetTimer((prev) => prev + 1); // 타이머 리셋
     } catch (error) {
-      setErrorMessage('인증번호 요청에 실패했습니다. 다시 시도해주세요.');
+      setErrorMessage('인증번호 발송에 실패했습니다.');
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const validationResult = validateCode(verificationCode); // 인증번호 유효성 검증
+
+    if (isInputDisabled) {
+      setErrorMessage('인증 시간이 만료되었습니다. 다시 시도해주세요.');
+      return;
+    }
+
+    // if (validationResult.isValid) {
+    if (verificationCode === '123456') {
+      navigate(ROUTES.ADMIN.STRATEGY.APPROVAL); // 관리자 전략 승인 페이지로 이동
+    } else {
+      setErrorMessage(validationResult.message);
     }
   };
   return (
     <div css={containerStyle}>
       <h3 css={pageHeadingStyle}>관리자 전용</h3>
-      <VerificationInput
-        value={verificationCode}
-        onChange={setVerificationCode}
-        isActive={isVerificationActive}
-        onTimeEnd={() => {
-          setErrorMessage('인증 시간이 만료되었습니다. 다시 시도해주세요.');
-        }}
-      />
-      {errorMessage && <p>{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <div css={emailVerifyContainer}>
+          <VerificationInput
+            value={verificationCode}
+            onChange={setVerificationCode}
+            resetTimer={resetTimer}
+            onTimeEnd={() => {
+              setIsInputDisabled(true); // 입력창 비활성화
+              setErrorMessage('인증 시간이 만료되었습니다. 다시 시도해주세요.');
+            }}
+            isDisabled={isInputDisabled}
+          />
+          <Button type='button' variant='accent' size='md' width={100} onClick={handleResend}>
+            재전송
+          </Button>
+        </div>
+
+        <Button
+          type='submit'
+          width={400}
+          css={buttonStyle}
+          disabled={!verificationCode || isInputDisabled}
+        >
+          확인
+        </Button>
+      </form>
+      {errorMessage && <p css={messageStyle}>{errorMessage}</p>}
     </div>
   );
 };
@@ -55,5 +113,25 @@ const pageHeadingStyle = css`
   line-height: ${theme.typography.lineHeights.md};
   font-weight: ${theme.typography.fontWeight.bold};
   margin-bottom: 32px;
+`;
+
+const emailVerifyContainer = css`
+  display: flex;
+  gap: 12px;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  margin-bottom: 12px;
+`;
+
+const buttonStyle = css`
+  margin-top: 24px;
+`;
+const messageStyle = css`
+  margin-top: 16px;
+  text-align: center;
+  color: ${theme.colors.main.alert};
+  font-size: ${theme.typography.fontSizes.caption};
+  line-height: ${theme.typography.lineHeights.sm};
 `;
 export default AdminVerifyPage;
