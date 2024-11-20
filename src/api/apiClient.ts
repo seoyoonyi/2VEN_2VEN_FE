b9e7049ce14c11d0aab46cd84aabc10ae95331ff
jizero <jizero27@gmail.com>
2024-11-20 15:27:54 +0900

    settings: apiClient.ts 주석 추가 (#134)

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -3,24 +3,28 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/authStore';
 
 export const apiClient = axios.create();
-// MSW와 실제 API를 구분하기 위한 baseURL 설정
+
+// TODO: MSW와 실제 API를 구분하기 위한 baseURL 설정(api 다 나오면 이걸로 변경..)
 // export const API_BASE_URL =
 //   import.meta.env.VITE_ENABLE_MSW === 'true'
 //     ? '' // MSW 사용 시 빈 문자열
 //     : import.meta.env.VITE_BASE_URL;
 
+// export const apiClient = axios.create({
+//   baseURL: API_BASE_URL,
+// });
+
 // TODO: 요청 인터셉터 구현
 // - 요청 URL과 메서드 로깅 (디버깅용)
 // - 엔드포인트 요청 시 헤더에 useMock:true추가 --> msw 데이터 , 추가 안하면 리얼 api
 // - Authorization 헤더에 로그인 토큰 추가 (localStorage or sessionStorage에서 가져오기)
+
 apiClient.interceptors.request.use((config) => {
   if (config.headers.useMock) {
     config.baseURL = '';
     delete config.headers.useMock;
-    console.log('mockup url: ', config.baseURL);
   } else {
     config.baseURL = import.meta.env.VITE_BASE_URL;
-    console.log('realAPI url: ', config.baseURL);
   }
   const { token } = useAuthStore.getState();
   if (token) {
