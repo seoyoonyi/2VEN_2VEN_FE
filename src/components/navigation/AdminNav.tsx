9ea3569d9039e6041c956ba3afcef91c30ed91bb
seoyoonyi <yiseoyoon@gmail.com>
2024-11-22 18:27:38 +0900

    design: admin 레이아웃 정리 (#166)

--- src/components/navigation/AdminNav.tsx
+++ src/components/navigation/AdminNav.tsx
@@ -1,53 +1,42 @@
 import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
+
+import NavigationMenu from '../common/NavigationMenu';
 
 import { ROUTES } from '@/constants/routes';
 import theme from '@/styles/theme';
 
-const AdminNav = () => (
-  <div css={navWrapper}>
-    <ul css={navStyle}>
-      <li css={navMenu}>
-        <Link to={`${ROUTES.ADMIN.STRATEGY.APPROVAL}`}>전략승인관리</Link>
-      </li>
-      <li css={navMenu}>
-        <Link to={`${ROUTES.ADMIN.STOCK_TYPE.LIST}`}>상품유형관리</Link>
-      </li>
-      <li css={navMenu}>
-        <Link to={`${ROUTES.ADMIN.TRADING_TYPE.LIST}`}>매매유형관리</Link>
-      </li>
-    </ul>
-  </div>
-);
+const AdminNav = () => {
+  const TraderMyPageNavItems = [
+    {
+      to: `${ROUTES.ADMIN.STRATEGY.APPROVAL}`,
+      label: '전략승인관리',
+      notificationCount: 1,
+    },
+    {
+      to: `${ROUTES.ADMIN.STOCK_TYPE.LIST}`,
+      label: '상품유형관리',
+    },
+    {
+      to: `${ROUTES.ADMIN.TRADING_TYPE.LIST}`,
+      label: '매매유형관리',
+    },
+  ];
 
-const navWrapper = css`
+  return (
+    <div css={navContainerStyle}>
+      <NavigationMenu items={TraderMyPageNavItems} />
+    </div>
+  );
+};
+
+const navContainerStyle = css`
   display: flex;
+  flex-direction: column;
   padding: 48px 20px;
   width: 305px;
   height: 285px;
-  gap: 8px;
   border-radius: 8px;
   background-color: ${theme.colors.main.white};
-`;
-const navStyle = css`
-  display: flex;
-  flex-direction: column;
-`;
-const navMenu = css`
-  display: flex;
-  width: 265px;
-  padding: 20px;
-  align-items: center;
-  gap: 8px;
-  font-size: 18px;
-  color: ${theme.colors.gray[500]};
-  font-weight: ${theme.typography.fontWeight.bold};
-  line-height: ${theme.typography.lineHeights.sm};
-
-  &:hover {
-    background-color: ${theme.colors.gray[50]};
-    color: ${theme.colors.main.black};
-  }
 `;
 
 export default AdminNav;
