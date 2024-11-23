b1089915fe752891f8e0775b7e39d31f79ea78a1
seoyoonyi <yiseoyoon@gmail.com>
2024-11-23 11:21:52 +0900

    design: 투자자 마이페이지 스타일링 재조정 (#166)

--- src/pages/mypage/investor/InvestorMyPage.tsx
+++ src/pages/mypage/investor/InvestorMyPage.tsx
@@ -3,7 +3,7 @@ import { css } from '@emotion/react';
 import theme from '@/styles/theme';
 
 const InvestorMyPage = () => (
-  <>
+  <div css={myPageWrapperStyle}>
     <div css={myPageHeaderStyle}>
       <div>
         <h2>나의 관심 전략</h2>
@@ -13,8 +13,15 @@ const InvestorMyPage = () => (
       </div>
     </div>
     <div></div>
-  </>
+  </div>
 );
+
+const myPageWrapperStyle = css`
+  width: 955px;
+  padding: 48px 40px 80px 40px;
+  background-color: ${theme.colors.main.white};
+  border-radius: 8px;
+`;
 
 const myPageHeaderStyle = css`
   margin-bottom: 40px;
