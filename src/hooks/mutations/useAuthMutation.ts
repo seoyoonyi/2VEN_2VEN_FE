4ea3505f77cf16ca9c2b6b157388d437cb27aa77
Hyejin <hyejin.frontend@gmail.com>
2024-11-25 03:08:38 +0900

    feat: 로그인 성공한 경우, 역할에 따른 헤더 노출 (#173)
    - useAuthMutation.ts에서 관리자의 경우 관리자 스토어에 데이터를 업데이트 하는 로직 추가
    - Header 컴포넌트 좌측 렌더링 부분에서,
        관리자인 경우 관리자 인증버튼 노출
        투자자 또는 트레이더의 경우 헤더 좌측에 프로필 아바타 노출

--- src/hooks/mutations/useAuthMutation.ts
+++ src/hooks/mutations/useAuthMutation.ts
@@ -2,11 +2,13 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { signin } from '@/api/auth';
+import { useAdminAuthStore } from '@/stores/adminAuthStore';
 import { useAuthStore } from '@/stores/authStore';
-import { SigninRequest, SigninResponse } from '@/types/auth';
+import { isAdminUser, SigninRequest, SigninResponse } from '@/types/auth';
 
 export const useSigninMutation = () => {
   const { setAuth } = useAuthStore();
+  const { setAdminAuth } = useAdminAuthStore();
   const queryClient = useQueryClient();
 
   return useMutation<SigninResponse, AxiosError, SigninRequest>({
@@ -17,13 +19,21 @@ export const useSigninMutation = () => {
         const response = await signin(credentials);
         console.log('Signin response:', response);
 
-        if (!response.data?.token) {
-          throw new Error('토큰이 없습니다.');
-        }
-        if (!response.data?.user) {
-          throw new Error('사용자 정보가 없습니다.');
+        if (!response.data?.token || !response.data?.user) {
+          throw new Error('인증 정보가 올바르지 않습니다.');
         }
 
+        // 관리자의 경우, 관리자 스토어도 업데이트
+        if (isAdminUser(response.data.user)) {
+          setAdminAuth({
+            is_authorized: response.data.user.is_authorized,
+            authorization_status: response.data.user.authorization_status,
+            authorized_at: response.data.user.authorized_at,
+            expires_at: response.data.user.expires_at,
+          });
+        }
+
+        // 공통 인증정보 저장
         setAuth(response.data.token, response.data.user);
         return response;
       } catch (error) {
