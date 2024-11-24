be1cbc46e761afe6f5be717751aaba5a7f7c1c74
Hyejin <hyejin.frontend@gmail.com>
2024-11-25 05:47:50 +0900

    feat: 이메일 인증요청, 인증코드 확인 리액트쿼리 적용 (#173)
    - 관리자인증 페이지가 마운트 되면 이메일 인증요청 되며 카운트 시작
    - 재요청 버튼 클릭 시 모두 초기화되며 새롭게 이메일인증 요청, 카운트 시작
    - 인증번호 입력 후 확인버튼 클릭하면, 성공 시 관리자 전략페이지로 이동
        - 실패 시 에러메시지 출력

--- src/pages/auth/admin/AdminVerifyPage.tsx
+++ src/pages/auth/admin/AdminVerifyPage.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
 
 import { css } from '@emotion/react';
+import { AxiosError } from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 import tickImage from '@/assets/images/tick.svg';
 import Button from '@/components/common/Button';
 import VerificationInput from '@/components/page/signup/VerificationInput';
 import { ROUTES } from '@/constants/routes';
+import {
+  useRequestVerificationMutation,
+  useVerifyAdminCodeMutation,
+} from '@/hooks/mutations/useVerifacationMutation';
 import theme from '@/styles/theme';
 import { validateCode } from '@/utils/validation';
 
@@ -15,14 +20,28 @@ const AdminVerifyPage = () => {
   const [verificationCode, setVerificationCode] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>('');
   const [resetTimer, setResetTimer] = useState<number>(0); // 타이머 리셋을 위한 상태
+  const [shouldReset, setShouldReset] = useState(false);
   const [isInputDisabled, setIsInputDisabled] = useState(false);
-  const [serverVerificationCode, setServerVerificationCode] = useState<string>(''); // 서버에서 받은 인증번호 저장
+  const [isVerificationActive, setIsVerificationActive] = useState(true);
+  const [serverVerificationCode, setServerVerificationCode] = useState<string>(''); // 서버에서 받은 인증번호 저장s
   const [isVerificationRequested, setIsVerificationRequested] = useState(false); // 추가된 상태
 
+  const { mutate: requestVerificationCode } = useRequestVerificationMutation();
+  const { mutate: verifyCode } = useVerifyAdminCodeMutation();
   // 페이지 마운트 시 최초 인증번호 요청
   useEffect(() => {
     handleResend();
   }, []);
+
+  // 재전송 처리를 위한 useEffect
+  useEffect(() => {
+    if (shouldReset) {
+      setErrorMessage('');
+      setResetTimer((prev) => prev + 1);
+      setIsInputDisabled(false); // 입력창 활성화
+      setShouldReset(false);
+    }
+  }, [shouldReset]);
 
   // verificationCode가 변경될 때마다 실행
   useEffect(() => {
@@ -32,20 +51,27 @@ const AdminVerifyPage = () => {
     }
   }, [verificationCode]);
 
-  // 인증번호 요청/재요청
+  // 인증번호 재요청
   const handleResend = () => {
+    setShouldReset(true);
     // setState는 비동기이므로, 이 시점에서는 아직 errorMessage가 변경되지 않았음
     try {
-      // const response = await requestVerificationEmail();
-      // setServerVerificationCode(response.verificationCode); // 서버 응답에서 인증번호 저장
-      // 테스트용 코드
-      setServerVerificationCode('123456'); // 테스트를 위해 하드코딩
-      setIsVerificationRequested(true); // 인증 요청 상태 활성화
-      // 인증번호 요청 성공 시, isVerificationActive를 토글 => 타이머 재시작!
-      setResetTimer((prev) => prev + 1); // 타이머 리셋
-      setIsInputDisabled(false);
-      setErrorMessage('');
-      setVerificationCode('');
+      // 이메일로 인증번호 요청 API 호출
+      requestVerificationCode(undefined, {
+        onSuccess: () => {
+          setVerificationCode(''); // 인증번호 초기화
+          setErrorMessage(''); // 에러메시지 초기화
+          setIsVerificationActive(true); // 인증 활성화
+          setResetTimer((prev) => prev + 1); // 타이머 리셋
+          setIsInputDisabled(false); // 입력창 활성화
+          setIsVerificationRequested(true); // 타이머 시작을 위한 상태 활성화 추가
+        },
+        onError: () => {
+          setErrorMessage('인증번호 발송에 실패했습니다.');
+          setIsVerificationActive(false); // 인증 비활성화
+          setIsVerificationRequested(false); // 타이머 시작을 위한 상태 비활성화 추가
+        },
+      });
     } catch (error) {
       setErrorMessage('인증번호 발송에 실패했습니다.');
     }
@@ -60,21 +86,26 @@ const AdminVerifyPage = () => {
       return;
     }
 
-    // 1. 유효성 검사 통과 여부
-    // 2. 서버에서 받은 인증번호와 일치 여부
-    // 조건을 단계별로 체크
-    if (validationResult.isValid) {
-      // 6자리 숫자 형식은 맞지만, 서버의 인증번호와 다른 경우
-      if (verificationCode !== serverVerificationCode) {
-        setErrorMessage('올바른 인증번호가 아닙니다.');
-      } else {
-        // 모든 조건 충족
-        navigate(ROUTES.ADMIN.STRATEGY.APPROVAL);
-      }
-    } else {
-      // 6자리 숫자 형식이 아닌 경우
+    if (!validationResult.isValid) {
       setErrorMessage(validationResult.message);
+      return;
     }
+    verifyCode(verificationCode, {
+      onSuccess: (response) => {
+        if (response.status === 'success') {
+          navigate(ROUTES.ADMIN.STRATEGY.APPROVAL, { replace: true });
+        } else {
+          setErrorMessage('인증에 실패했습니다. 다시 시도해주세요.');
+        }
+      },
+      onError: (error: AxiosError) => {
+        if (error.response?.status === 401) {
+          setErrorMessage('올바른 인증번호가 아닙니다.');
+        } else {
+          setErrorMessage('인증 처리 중 오류가 발생했습니다.');
+        }
+      },
+    });
   };
   return (
     <div css={containerStyle}>
