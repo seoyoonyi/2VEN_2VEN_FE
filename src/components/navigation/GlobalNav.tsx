eb17c4a63813370f879f248dcbc6c13d473beb27
Hyejin <hyejin.frontend@gmail.com>
2024-11-25 03:12:51 +0900

    feat: 관리자 로그인 성공 시 보여지는 GNB 로직 추가 (#173)
    - 사용자의 role이 관리자인지 확인
    - 사용자의 role이 관리자이고 관리자 인증을 완료했는지 확인
    - 관리자가 로그인 성공 후 메인페이지 상단에 관리자(비활성화) 링크가 나타남
    - 관리자가 관리자 인증페이지에서 인증 성공 후 메인페이지 상단에 관리자 링크가 활성화됨

--- src/components/navigation/GlobalNav.tsx
+++ src/components/navigation/GlobalNav.tsx
@@ -1,20 +1,48 @@
 import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { ROUTES } from '@/constants/routes';
+import { useAdminAuthStore } from '@/stores/adminAuthStore';
+import { useAuthStore } from '@/stores/authStore';
+import theme from '@/styles/theme';
+import { isAdminUser } from '@/types/auth';
+import { withOpacity } from '@/utils/color';
 
-const GlobalNav = () => (
-  <nav css={navStyle}>
-    <ul>
-      <li>
-        <Link to={ROUTES.STRATEGY.LIST}>전략</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.TRADER.LIST}>트레이더</Link>
-      </li>
-    </ul>
-  </nav>
-);
+const GlobalNav = () => {
+  const location = useLocation();
+  const { user } = useAuthStore();
+  const { adminAuth } = useAdminAuthStore();
+  const isAdmin = user && isAdminUser(user); // 사용자의 role이 ROLE_ADMIN인지 확인
+  const isAuthorizedAdmin = adminAuth?.is_authorized; // 사용자가 ROLE_ADMIN이고 adminAuth의 is_authorized가 true인지 확인
+
+  return (
+    <nav css={navStyle}>
+      <ul>
+        <li>
+          <Link to={ROUTES.STRATEGY.LIST}>전략</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.TRADER.LIST}>트레이더</Link>
+        </li>
+        {isAdmin && (
+          <li css={!isAuthorizedAdmin && disabledLinkStyle}>
+            <Link
+              to={isAuthorizedAdmin ? ROUTES.ADMIN.STRATEGY.APPROVAL : '#'}
+              onClick={(e) => {
+                if (!isAuthorizedAdmin) {
+                  e.preventDefault();
+                }
+              }}
+              title={!isAuthorizedAdmin ? '관리자 인증이 필요합니다.' : ''}
+            >
+              관리자
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 const navStyle = css`
   display: flex;
@@ -24,4 +52,11 @@ const navStyle = css`
   }
 `;
 
+const disabledLinkStyle = css`
+  a {
+    color: ${withOpacity.disabled(theme.colors.main.black)};
+    cursor: not-allowed;
+    pointer-events: none;
+  }
+`;
 export default GlobalNav;
