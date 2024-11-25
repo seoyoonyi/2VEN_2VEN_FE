1cc034e6f5f685ea7c41077e71c168b6d5a31840
Hyejin <hyejin.frontend@gmail.com>
2024-11-26 01:16:54 +0900

    fix: 이메일 인증요청, 인증확인 파라미터 추가 (#223)
    - 이메일 인증요청, 인증확인 파라미터 추가
    - 이메일 인증요청 시 이메일을 파라미터로 전달
    - 이메일 인증확인 시 이메일, 인증번호를 파라미터로 전달
    - 현재 500 에러 발생
    - 비밀번호 찾기페이지와, 관리자 이메일 인증페이지에 적용

--- src/hooks/mutations/useVerifacationMutation.tsx
+++ src/hooks/mutations/useVerifacationMutation.tsx
@@ -6,12 +6,12 @@ import { ApiResponse } from '@/types/auth';
 
 // 인증번호 요청 mutation
 export const useRequestVerificationMutation = () =>
-  useMutation<ApiResponse<null>, AxiosError>({
+  useMutation<ApiResponse<null>, AxiosError, string>({
     mutationFn: requestVerificationCode,
   });
 
 // 인증번호 검증 mutation
 export const useVerifyAdminCodeMutation = () =>
-  useMutation<ApiResponse<{ expires_at: string }>, AxiosError, string>({
-    mutationFn: verifyCode,
+  useMutation<ApiResponse<{ expires_at: string }>, AxiosError, { email: string; code: string }>({
+    mutationFn: ({ email, code }) => verifyCode(email, code),
   });
