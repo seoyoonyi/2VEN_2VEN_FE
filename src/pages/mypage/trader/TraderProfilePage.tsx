e795f2cce2caffdcadbceceb8230d8ffec2bc9b1
seoyoonyi <yiseoyoon@gmail.com>
2024-11-26 23:04:48 +0900

    design: 회원탈퇴 버튼 추가 (#201)

--- src/pages/mypage/trader/TraderProfilePage.tsx
+++ src/pages/mypage/trader/TraderProfilePage.tsx
@@ -1,3 +1,7 @@
+import { css } from '@emotion/react';
+import { MdOutlineNoAccounts } from 'react-icons/md';
+
+import Button from '@/components/common/Button';
 import PasswordChange from '@/components/page/mypage/PasswordChange';
 import ProfileManager from '@/components/page/mypage/ProfileManager';
 
@@ -5,7 +9,19 @@ const TraderProfilePage = () => (
   <>
     <ProfileManager />
     <PasswordChange />
+    <Button variant='ghostGray' customStyle={logoutStyle} size='sm'>
+      <MdOutlineNoAccounts size={16} />
+      <span>회원 탈퇴</span>
+    </Button>
   </>
 );
+
+const logoutStyle = css`
+  width: auto;
+  justify-content: flex-start;
+  margin-top: 16px;
+  padding: 8px 20px;
+  gap: 4px;
+`;
 
 export default TraderProfilePage;
