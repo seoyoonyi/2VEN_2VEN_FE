4383a5935b87768810e2cd1b565e195fad6715d6
Panda-racoon <millie3519@gmail.com>
2024-12-01 19:44:52 +0900

    feat: 문의등록 post api연결

--- src/hooks/mutations/useSubmitInquiryCreate.ts
+++ src/hooks/mutations/useSubmitInquiryCreate.ts
@@ -3,25 +3,24 @@ import { useNavigate } from 'react-router-dom';
 
 import { submitInquiryCreate } from '@/api/inquiryCreate';
 import { ROUTES } from '@/constants/routes';
-import { InquiryCreateData, InquiryCreateResponce } from '@/types/inquiryCreate';
+import { InquiryCreateData, InquiryCreateResponse } from '@/types/inquiryCreate';
 
-export const useSubmitConsultationCreate = () => {
+export const useSubmitInquiryCreate = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
-  return useMutation<
-    InquiryCreateResponce,
-    Error,
-    { investorId: string; payload: InquiryCreateData }
-  >({
-    mutationFn: async ({ investorId, payload }) => submitInquiryCreate(investorId, payload), // API 호출 함수
+  return useMutation<InquiryCreateResponse, Error, { payload: InquiryCreateData }>({
+    mutationFn: async ({ payload }) =>
+      submitInquiryCreate({
+        ...payload,
+      }),
     onSuccess: (data) => {
       console.log('문의 등록 성공:', data);
 
       // 성공 후 목록 페이지로 이동
       navigate(ROUTES.MYPAGE.INVESTOR.MYINQUIRY.LIST);
       window.scrollTo(0, 0);
-      queryClient.invalidateQueries({ queryKey: ['inquiries'] }); // 캐시 무효화
+      queryClient.invalidateQueries({ queryKey: ['inquiries'] });
     },
     onError: (error) => {
       console.error('문의 등록 실패:', error);
--- src/types/inquiryCreate.ts
+++ src/types/inquiryCreate.ts
@@ -2,24 +2,8 @@ export type Status = 'COMPLETED' | 'PENDING';
 
 // 문의등록
 export interface InquiryCreateData {
-  strategyId: number;
-  strategyName: string;
-  investmentAmount: number;
-  investmentDate: string;
-  title: string;
-  content: string;
-  status: string;
-}
-
-// 문의 등록 반환 값
-export interface InquiryCreateResponce {
-  id: number;
   investorId: string;
-  investorName: string;
-  investorProfileUrl: string | null;
   traderId: string;
-  traderName: string;
-  traderProfileUrl: string | null;
   strategyId: number;
   strategyName: string;
   investmentAmount: number;
@@ -27,12 +11,24 @@ export interface InquiryCreateResponce {
   title: string;
   content: string;
   status: Status;
+}
+
+// 문의 등록 반환 값
+export interface InquiryCreateResponse {
+  id: number;
   createdAt: string;
   updatedAt: string;
   replyContent: string | null;
   answerDate: string | null;
   replyCreatedAt: string | null;
   replyUpdatedAt: string | null;
+  investorId: string;
+  traderId: string;
+  strategyId: number;
+  strategyName: string;
+  investmentAmount: number;
+  investmentDate: string;
+  title: string;
+  content: string;
+  status: Status;
 }
-
-// 전략 폼 요청
