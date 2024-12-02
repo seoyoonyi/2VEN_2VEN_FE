ec39da2ecdcc01a2ec092071f08d1ff43530d4b6
Hyejin <hyejin.frontend@gmail.com>
2024-12-02 13:24:39 +0900

    feat: 비밀번호 재설정 페이지 구현 및 api 연동 (#269)
    확인 버튼 누르면,
    - 새 비밀번호 등록 입력창 유효성 검사
        - 유효성 검사 실패하면 입력창 하단에 에러메시지 출력
    - 새 비밀번호 등록과 새 비밀번호 확인, 두 입력이 동일한지 검사
        - 검사 실패하면 입력창 하단에 에러메시지 출력
    - 입력창 비워지면 에러메시지 사라짐
    - 재설정 요청 api 호출하고,
        - 호출 성공하면, 비밀번호 재설정 성공 페이지로 이동
        - 호출 실패하면 에러메시지

--- src/pages/auth/find/ResetPasswordPage.tsx
+++ src/pages/auth/find/ResetPasswordPage.tsx
@@ -1,46 +1,136 @@
+import { useState } from 'react';
+
 import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import tickImage from '@/assets/images/tick.svg';
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
 import { ROUTES } from '@/constants/routes';
+import { useResetPasswordMutation } from '@/hooks/mutations/useResetPasswordMutation';
 import theme from '@/styles/theme';
-const ResetPasswordPage = () => (
-  <div css={containerStyle}>
-    <h3 css={pageHeadingStyle}>비밀번호 재설정</h3>
-    <p css={infoStyle}>
-      <img src={tickImage} alt='tick' />
-      <span>영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요</span>
-    </p>
-    <form css={formStyle}>
-      <Input
-        type='password'
-        inputSize='lg'
-        required
-        name='newpassword'
-        placeholder='새 비밀번호 등록'
-        css={inputStyle}
-      />
-      <Input
-        type='password'
-        inputSize='lg'
-        required
-        name='newpassword-confirm'
-        placeholder='새 비밀번호 확인'
-      />
-      <Button type='submit' width={400} disabled>
-        완료
-      </Button>
-    </form>
-    <ul css={findEmailLinkStyle}>
-      <li>이메일이 기억나지 않나요?</li>
-      <li>
-        <Link to={ROUTES.AUTH.FIND.EMAIL}>이메일 찾기</Link>
-      </li>
-    </ul>
-  </div>
-);
+import { isValidPassword } from '@/utils/validation';
+const ResetPasswordPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const email = location.state?.email;
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { mutate: resetPassword } = useResetPasswordMutation();
+
+  // 비밀번호 유효성 검사
+  const validatePassword = (newPassword: string) => {
+    const { isValid, message } = isValidPassword(newPassword);
+    if (!isValid) {
+      setErrorMessage(message);
+    }
+    return isValid;
+  };
+
+  // 비밀번호 일치 여부 검사
+  const validatePasswordMatch = () => {
+    if (newPassword !== confirmPassword) {
+      setErrorMessage('비밀번호가 일치하지 않습니다.');
+      return false;
+    }
+    return true;
+  };
+
+  // 폼 제출 처리
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 기존 에러메시지 초기화
+    setErrorMessage('');
+
+    // 입력값이 없는 경우 처리
+    if (!newPassword) {
+      setErrorMessage('비밀번호를 입력해주세요.');
+      return;
+    }
+    if (!confirmPassword) {
+      setErrorMessage('비밀번호를 확인해주세요.');
+      return;
+    }
+
+    // 유효성 검사는 제출 시에만 진행
+    const isPasswordValid = validatePassword(newPassword);
+    if (!isPasswordValid) {
+      return;
+    }
+    const isPasswordMatch = validatePasswordMatch();
+    if (!isPasswordMatch) {
+      return;
+    }
+
+    const requestBody = { email, newPassword, confirmPassword };
+    console.log('Request body:', requestBody); // 요청 바디 확인
+
+    resetPassword(
+      { email, newPassword, confirmPassword },
+      {
+        onSuccess: () => {
+          navigate(ROUTES.AUTH.FIND.PASSWORD_RESET_SUCCESS);
+        },
+      }
+    );
+  };
+
+  return (
+    <div css={containerStyle}>
+      <h3 css={pageHeadingStyle}>비밀번호 재설정</h3>
+      <p css={infoStyle}>
+        <img src={tickImage} alt='tick' />
+        <span>영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요</span>
+      </p>
+      <form css={formStyle} onSubmit={handleSubmit}>
+        <Input
+          type='password'
+          inputSize='lg'
+          required
+          value={newPassword}
+          name='newPassword'
+          placeholder='새 비밀번호 등록'
+          css={inputStyle}
+          onChange={(e) => {
+            setNewPassword(e.target.value); // 새 비밀번호 입력값 변경
+            if (e.target.value === '') {
+              setErrorMessage(''); // 입력값이 없는 경우 에러메시지 초기화
+            }
+          }}
+          status={errorMessage ? 'error' : 'default'}
+        />
+        <Input
+          type='password'
+          inputSize='lg'
+          required
+          value={confirmPassword}
+          name='conformPassword'
+          placeholder='새 비밀번호 확인'
+          onChange={(e) => {
+            setConfirmPassword(e.target.value); // 새 비밀번호 확인 입력값 변경
+            if (e.target.value === '') {
+              setErrorMessage(''); // 입력값이 없는 경우 에러메시지 초기화
+            }
+          }}
+          status={errorMessage ? 'error' : 'default'}
+        />
+        {errorMessage && <p css={messageStyle}>{errorMessage}</p>}
+        <Button type='submit' width={400} disabled={!newPassword || !confirmPassword}>
+          완료
+        </Button>
+      </form>
+      <ul css={findEmailLinkStyle}>
+        <li>이메일이 기억나지 않나요?</li>
+        <li>
+          <Link to={ROUTES.AUTH.FIND.EMAIL}>이메일 찾기</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 const containerStyle = css`
   display: flex;
@@ -117,5 +207,12 @@ const findEmailLinkStyle = css`
       display: none;
     }
   }
+`;
+const messageStyle = css`
+  margin-top: 16px;
+  text-align: center;
+  color: ${theme.colors.main.alert};
+  font-size: ${theme.typography.fontSizes.caption};
+  line-height: ${theme.typography.lineHeights.sm};
 `;
 export default ResetPasswordPage;
