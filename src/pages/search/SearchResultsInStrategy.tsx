92ba9657d562856b47c29a5e6ad1da40e8a2ed11
Hyejin <hyejin.frontend@gmail.com>
2024-12-04 04:38:19 +0900

    feat: 키워드 전략 검색 상세 페이지 구현 (#308)
    - 한 페이지에 표시되는 키워드 전략 검색 결과 수를 25개로 options 설정
    - 페이지네이션 기능 추가

--- src/pages/search/SearchResultsInStrategy.tsx
+++ src/pages/search/SearchResultsInStrategy.tsx
@@ -1,7 +1,95 @@
-const SearchResultsInStrategy = () => (
-  <div>
-    <h1>전략 내 검색결과 페이지</h1>
-  </div>
-);
+import { useState } from 'react';
 
+import { css } from '@emotion/react';
+import { useSearchParams } from 'react-router-dom';
+
+import Loader from '@/components/common/Loading';
+import Pagination from '@/components/common/Pagination';
+import SearchedStrategyList from '@/components/page/search/SearchedStrategyList';
+import { useSearchStrategies } from '@/hooks/queries/useSearch';
+import theme from '@/styles/theme';
+import { mapToStrategyData } from '@/utils/mappers';
+import { shortenString } from '@/utils/string';
+
+const SearchResultsInStrategy = () => {
+  const [searchParams] = useSearchParams();
+  const keyword = searchParams.get('keyword') || '';
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+
+  const { data: strategyResults, isLoading: isLoadingStrategies } = useSearchStrategies(keyword, {
+    page: currentPage - 1, // 페이지는 0부터 시작
+    pageSize: 25,
+  }); // 전략 검색 결과
+
+  const mappedStrategies = strategyResults?.data.map(mapToStrategyData) ?? [];
+  if (isLoadingStrategies) {
+    return <Loader />;
+  }
+  return (
+    <div css={containerStyle}>
+      <h2 css={pageHeadingStyle}>
+        <strong>&lsquo;{shortenString(keyword, 10)}&rsquo;&nbsp;</strong>
+        <span>에 대한 검색 결과</span>
+      </h2>
+      <div css={headerStyle}>
+        <div css={totalStyle}>
+          Total <span>{strategyResults?.totalElements ?? 0}</span>
+        </div>
+      </div>
+      <SearchedStrategyList strategies={mappedStrategies} />
+      <div css={paginationWrapperStyle}>
+        <Pagination
+          totalPage={strategyResults?.totalPages ?? 1}
+          limit={5}
+          page={currentPage}
+          setPage={setCurrentPage}
+        />
+      </div>
+    </div>
+  );
+};
+
+const containerStyle = css`
+  width: ${theme.layout.width.content};
+  margin: 0 auto;
+  padding: 40px 0;
+  margin-bottom: 76px;
+`;
+const pageHeadingStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 76px 0 64px;
+  background-color: ${theme.colors.gray[100]};
+  font-size: ${theme.typography.fontSizes.heading.h2};
+  line-height: ${theme.typography.lineHeights.md};
+  font-weight: ${theme.typography.fontWeight.bold};
+  strong {
+    color: ${theme.colors.main.black};
+  }
+  span {
+    color: ${theme.colors.gray[600]};
+  }
+`;
+const headerStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 64px;
+  margin-bottom: 24px;
+`;
+const totalStyle = css`
+  color: ${theme.colors.gray[700]};
+  font-weight: ${theme.typography.fontWeight.regular};
+  line-height: ${theme.typography.lineHeights.lg};
+
+  span {
+    color: ${theme.colors.main.primary};
+  }
+`;
+const paginationWrapperStyle = css`
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+`;
 export default SearchResultsInStrategy;
