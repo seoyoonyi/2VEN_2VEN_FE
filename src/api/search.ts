d57aef46067fa42165c2fd4a151f7797588dfe6c
Hyejin <hyejin.frontend@gmail.com>
2024-12-04 04:28:31 +0900

    feat: 상세추가 검색 api 추가 (#308)

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,6 +1,11 @@
 import apiClient from '@/api/apiClient';
 import { API_ENDPOINTS } from '@/api/apiEndpoints';
-import { SearchedStrategy, SearchedTrader, SearchResponse } from '@/types/search';
+import {
+  StrategySearchParams,
+  SearchedStrategy,
+  SearchedTrader,
+  SearchResponse,
+} from '@/types/search';
 
 interface SearchParams {
   keyword: string;
@@ -9,6 +14,7 @@ interface SearchParams {
   sortOption?: 'strategyCnt' | 'latestSignup';
 }
 
+// 키보드 입력에 따른 '전체 트레이더' 검색 결과를 보여주는 경우 - 트레이더 검색
 export const searchTraders = async ({
   keyword,
   page = 0,
@@ -27,16 +33,35 @@ export const searchTraders = async ({
   return data;
 };
 
+// 키보드 입력에 따른 '전체 전략' 검색 결과를 보여주는 경우 - 전략 검색
 export const searchStrategies = async ({
   keyword,
   page = 0,
-  pageSize = 6,
+  pageSize = 6, // 기본값은 6으로 유지 (전체 검색 결과 페이지용)
 }: SearchParams): Promise<SearchResponse<SearchedStrategy>> => {
   const { data } = await apiClient.get(API_ENDPOINTS.SEARCH.STRATEGIES, {
     params: {
       keyword,
       page,
       pageSize,
+    },
+  });
+  return data;
+};
+
+// 추가 검색 조건이 있는 경우 - 상세 검색(전략 검색)
+export const searchStrategyDetail = async ({
+  keyword,
+  page = 0, // 서버는 0-based pagination 사용
+  pageSize = 25,
+  ...params
+}: StrategySearchParams): Promise<SearchResponse<SearchedStrategy>> => {
+  const { data } = await apiClient.get(API_ENDPOINTS.SEARCH.STRATEGIES_DETAIL, {
+    params: {
+      keyword,
+      page: page - 1, // 서버는 0-based pagination 사용
+      pageSize,
+      ...params, // 추가 검색 조건(상세 필터 체크박스값들)
     },
   });
   return data;
