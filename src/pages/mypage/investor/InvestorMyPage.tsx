cde3cc1fb7a53a28e0896199cb7939aa766ec89a
Sumin <tnalsdldkr2@naver.com>
2024-12-05 02:07:30 +0900

    feat: 페이지 이동시 토스트 상태 초기화 (#277)

--- src/pages/mypage/investor/InvestorMyPage.tsx
+++ src/pages/mypage/investor/InvestorMyPage.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { css } from '@emotion/react';
 import { useNavigate } from 'react-router-dom';
 
@@ -39,6 +41,12 @@ const InvestorMyPage = () => {
   const { mutate: submitFolder } = useSubmitFolder();
   const { mutate: updateFolder } = useUpdateFolderName();
   const { mutate: deleteFolder } = useDeleteFolder();
+
+  useEffect(() => {
+    if (isToastVisible) {
+      hideToast();
+    }
+  }, [hideToast, isToastVisible]);
 
   const handleFolderList = (folderId: number) => {
     navigate(ROUTES.MYPAGE.INVESTOR.FOLLOWING.STRATEGIES(String(folderId)));
@@ -138,7 +146,7 @@ const InvestorMyPage = () => {
 
   if (isLoading) {
     return (
-      <div css={myPageWrapperStyle}>
+      <div>
         <Loader />
       </div>
     );
