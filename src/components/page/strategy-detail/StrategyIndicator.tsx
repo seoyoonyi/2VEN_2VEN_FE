3928264e74d8ad4fbaae0aff95be340adb16b728
jizero <jizero27@gmail.com>
2024-12-04 14:02:19 +0900

    fix : value 수정 (#251)

--- src/components/page/strategy-detail/StrategyIndicator.tsx
+++ src/components/page/strategy-detail/StrategyIndicator.tsx
@@ -4,36 +4,36 @@ import theme from '@/styles/theme';
 import { formatRate } from '@/utils/statistics';
 
 interface IndicatorProps {
-  cumulativeRate: number;
-  maximumRate: number;
-  avgProfit: number;
+  cumulativeProfitLossRate: number;
+  maxDrawdownRate: number;
+  averageProfitLossRate: number;
   profitFactor: number;
   winRate: number;
 }
 
 const StrategyIndicator = ({
-  cumulativeRate,
-  maximumRate,
-  avgProfit,
+  cumulativeProfitLossRate,
+  maxDrawdownRate,
+  averageProfitLossRate,
   profitFactor,
   winRate,
 }: IndicatorProps) => (
   <div css={indicatorWrapperStyle}>
-    {!cumulativeRate && !maximumRate && !avgProfit && !profitFactor ? (
+    {!cumulativeProfitLossRate && !maxDrawdownRate && !averageProfitLossRate && !profitFactor ? (
       <div>주요지표 데이터가 없습니다.</div>
     ) : (
       <>
         <div css={rateAreaStyle}>
           <div css={titleStyle}>누적수익률</div>
-          <div css={rateContentStyle}>{formatRate(cumulativeRate)}%</div>
+          <div css={rateContentStyle}>{formatRate(cumulativeProfitLossRate)}%</div>
         </div>
         <div css={rateAreaStyle}>
           <div css={titleStyle}>최대자본인하율</div>
-          <div css={rateContentStyle}>{formatRate(maximumRate)}%</div>
+          <div css={rateContentStyle}>{formatRate(maxDrawdownRate)}%</div>
         </div>
         <div css={rateAreaStyle}>
           <div css={titleStyle}>평균손익률</div>
-          <div css={rateContentStyle}>{formatRate(avgProfit)}%</div>
+          <div css={rateContentStyle}>{formatRate(averageProfitLossRate)}%</div>
         </div>
         <div css={rateAreaStyle}>
           <div css={titleStyle}>Profit Factor</div>
