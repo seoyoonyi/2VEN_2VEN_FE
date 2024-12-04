b392a2d57e1162c5aa6e66bb89ea980e0b1dfba7
jizero <jizero27@gmail.com>
2024-12-04 19:57:17 +0900

    fix : 반올림 제거 (#327)

--- src/utils/statistics.ts
+++ src/utils/statistics.ts
@@ -2,7 +2,7 @@
 export const formatLoss = (value: number) => value.toLocaleString();
 
 //23.23223 -> 23.23
-export const formatRate = (value: number) => Math.round(value * 100) / 100;
+export const formatRate = (value: number) => Math.floor(value * 100) / 100;
 
 export const formatValue = (key: string, value: string | number) => {
   if (typeof value === 'number') {
