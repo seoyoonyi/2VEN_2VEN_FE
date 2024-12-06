8e45d7e472768bbdbaed8fcd67b95de442a8f4a9
seoyoonyi <yiseoyoon@gmail.com>
2024-12-06 11:33:14 +0900

    feat: 회원 탈퇴기능 구현 (#345)

--- src/pages/auth/WithdrawalSuccessPage.tsx
+++ src/pages/auth/WithdrawalSuccessPage.tsx
@@ -1,7 +1,45 @@
-const WithdrawalSuccessPage = () => (
-  <div>
-    <h1>정상적으로 회원탈퇴 하였습니다.</h1>
-  </div>
-);
+import { css } from '@emotion/react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@/components/common/Button';
+import { ROUTES } from '@/constants/routes';
+import theme from '@/styles/theme';
+
+const WithdrawalSuccessPage = () => {
+  const navigate = useNavigate();
+  const handleMoveHomePage = () => {
+    navigate(ROUTES.HOME.PATH);
+  };
+  return (
+    <div css={containerStyle}>
+      <h2>탈퇴가 완료되었습니다</h2>
+      <p>그동안 서비스를 이용해주셔서 감사드리며, 더 나은 서비스로 다시 찾아뵙겠습니다.</p>
+      <Button onClick={handleMoveHomePage}>홈으로</Button>
+    </div>
+  );
+};
+
+const containerStyle = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - (240px + 94px));
+
+  h2 {
+    margin-bottom: 10px;
+    ${theme.textStyle.headings.h2}
+  }
+
+  p {
+    margin-bottom: 40px;
+  }
+
+  button {
+    width: 154px;
+    height: 60px;
+    padding: 20px 32px;
+  }
+`;
 
 export default WithdrawalSuccessPage;
