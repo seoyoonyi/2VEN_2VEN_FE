b04c97dfadd47c851b2a33efa6d726519463bbcb
jizero <jizero27@gmail.com>
2024-12-08 17:43:30 +0900

    fix: 거부 사유에 아바타 컴포넌트 적용, 일간분석 승인요청상태 데이터 3개이상 조건 삭제

--- src/pages/strategy/StrategyDetailPage.tsx
+++ src/pages/strategy/StrategyDetailPage.tsx
@@ -51,14 +51,12 @@ const StrategyDetailPage = () => {
   const { mutate: approveStrategy } = useStrategyDetailApprove();
   const { mutate: terminateStrategy } = useStrategyDetailTerminate();
   const { openModal } = useModalStore();
-  const { dailyAnalysis } = useFetchDailyAnalysis(Number(strategyId), 0, 5, role);
   const { data: approveState } =
     useFetchApproveState(Number(strategyId), role, {
       enabled: strategy?.isApproved === 'N',
     }) || '';
-  const isApproved =
-    (dailyAnalysis?.length >= 3 && strategy?.requestAvailable === true) ||
-    approveState?.isApproved === 'N';
+  const isApproved = strategy?.requestAvailable === true;
+  approveState?.isApproved === 'N';
   const isTerminated = strategy?.strategyStatusCode === 'STRATEGY_OPERATION_TERMINATED';
   const isOwner = user?.memberId === strategy?.memberId;
   const isAdmin = role === 'ROLE_ADMIN';
